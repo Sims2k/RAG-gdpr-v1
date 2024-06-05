1fde818dc37af44aa00a935f88a592638e1d83e1
Simon Bernbeck <Simon.Bernbeck@gmail.com>
2024-06-05 16:47:58 +0200

    Finall all packages installed

--- utils.py
+++ utils.py
@@ -1,71 +1,78 @@
 import os
-from dotenv import load_dotenv, find_dotenv
+
+# from dotenv import load_dotenv, find_dotenv
+from dotenv import load_dotenv
 
 import numpy as np
-from trulens_eval import (
-    Feedback,
-    TruLlama,
-    OpenAI
-)
+from trulens_eval import Feedback, TruLlama, OpenAI
 
 from trulens_eval.feedback import Groundedness
 import nest_asyncio
 
 nest_asyncio.apply()
 
+load_dotenv()
+
 
 def get_openai_api_key():
-    _ = load_dotenv(find_dotenv())
-
+    # Directly return the API key using the already loaded environment variables
     return os.getenv("OPENAI_API_KEY")
 
 
-def get_hf_api_key():
-    _ = load_dotenv(find_dotenv())
+print("OPENAI_API_KEY: ", get_openai_api_key())
 
+
+def get_hf_api_key():
+    # Directly return the API key using the already loaded environment variables
     return os.getenv("HUGGINGFACE_API_KEY")
+
+
+# def get_openai_api_key():
+#     _ = load_dotenv(find_dotenv())
+
+#     return os.getenv("OPENAI_API_KEY")
+
+
+# def get_hf_api_key():
+#     _ = load_dotenv(find_dotenv())
+
+#     return os.getenv("HUGGINGFACE_API_KEY")
 
 openai = OpenAI()
 
-qa_relevance = (
-    Feedback(openai.relevance_with_cot_reasons, name="Answer Relevance")
-    .on_input_output()
-)
+qa_relevance = Feedback(
+    openai.relevance_with_cot_reasons, name="Answer Relevance"
+).on_input_output()
 
 qs_relevance = (
-    Feedback(openai.relevance_with_cot_reasons, name = "Context Relevance")
+    Feedback(openai.relevance_with_cot_reasons, name="Context Relevance")
     .on_input()
     .on(TruLlama.select_source_nodes().node.text)
     .aggregate(np.mean)
 )
 
-#grounded = Groundedness(groundedness_provider=openai, summarize_provider=openai)
+# grounded = Groundedness(groundedness_provider=openai, summarize_provider=openai)
 grounded = Groundedness(groundedness_provider=openai)
 
 groundedness = (
     Feedback(grounded.groundedness_measure_with_cot_reasons, name="Groundedness")
-        .on(TruLlama.select_source_nodes().node.text)
-        .on_output()
-        .aggregate(grounded.grounded_statements_aggregator)
+    .on(TruLlama.select_source_nodes().node.text)
+    .on_output()
+    .aggregate(grounded.grounded_statements_aggregator)
 )
 
 feedbacks = [qa_relevance, qs_relevance, groundedness]
 
+
 def get_trulens_recorder(query_engine, feedbacks, app_id):
-    tru_recorder = TruLlama(
-        query_engine,
-        app_id=app_id,
-        feedbacks=feedbacks
-    )
+    tru_recorder = TruLlama(query_engine, app_id=app_id, feedbacks=feedbacks)
     return tru_recorder
 
+
 def get_prebuilt_trulens_recorder(query_engine, app_id):
-    tru_recorder = TruLlama(
-        query_engine,
-        app_id=app_id,
-        feedbacks=feedbacks
-        )
+    tru_recorder = TruLlama(query_engine, app_id=app_id, feedbacks=feedbacks)
     return tru_recorder
+
 
 from llama_index import ServiceContext, VectorStoreIndex, StorageContext
 from llama_index.node_parser import SentenceWindowNodeParser
